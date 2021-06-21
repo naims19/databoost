336f4e3610a056de255cf92cdf5c0e0eeb893e53
naim imran <m.naim.imran@gmail.com>
2021-06-21 11:23:59 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  @ViewChild(IonContent) content: IonContent;
+
   constructor() {}
+
+  scrollTo(_id: string) {
+    let y = document.getElementById(_id).offsetTop-100;
+    this.content.scrollToPoint(0, y, 1000);
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  @ViewChild (IonContent) content: IonContent;
+
   constructor() {}
 
+  scrollTo(_id: string) {
+    let y = document.getElementById(_id).offsetTop;
+    this.content.scrollToPoint(0,y,1000);
+  }
+
+   myFunction() {
+     console.log('test');
+    let x = document.getElementById('myLinks');
+    let z = document.getElementById('test');
+    if (x.style.display === 'block') {
+      x.style.display = 'none';
+      z.style.display= 'block';
+    } else {
+      x.style.display = 'block';
+      z.style.display = 'none';
+    }
+  }
+
+  buttonTest(){
+    console.log('woi');
+    let z = document.getElementById('test');
+    let x = document.getElementById('myLinks');
+    if (z.style.display === 'block') {
+      z.style.display = 'block';
+      x.style.display = 'none';
+    } else {
+      z.style.display = 'none';
+      x.style.display= 'block';
+    }
+  }
 }
